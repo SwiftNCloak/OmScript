d9bb2d0ec6f55dc2689802af2b34ed1adbb75b8b
SwiftNCloak <markyjamesbonifacio@gmail.com>
2023-01-20 09:37:28 +0800

    Added new background and foreground to make the console look neat

--- shell.py
+++ shell.py
@@ -1,4 +1,5 @@
 import oms
+from colorama import Fore, Back, Style
 
 class bcolors:
     HEADER = '\033[95m'
@@ -11,9 +12,36 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
+class fg:
+    black = '\033[30m'
+    red = '\033[31m'
+    green = '\033[32m'
+    orange = '\033[33m'
+    blue = '\033[34m'
+    purple = '\033[35m'
+    cyan = '\033[36m'
+    lightgrey = '\033[37m'
+    darkgrey = '\033[90m'
+    lightred = '\033[91m'
+    lightgreen = '\033[92m'
+    yellow = '\033[93m'
+    lightblue = '\033[94m'
+    pink = '\033[95m'
+    lightcyan = '\033[96m'
+
+class bg:
+    black = '\033[40m'
+    red = '\033[41m'
+    green = '\033[42m'
+    orange = '\033[43m'
+    blue = '\033[44m'
+    purple = '\033[45m'
+    cyan = '\033[46m'
+    lightgrey = '\033[47m'
+
 while True:
-    text = input('oms> ')
+    text = input(bg.orange + fg.black + 'oms>' + bcolors.ENDC + ' ')
     result, error = oms.run('<stdin>', text)
 
-    if error: print(bcolors.WARNING + error.as_string() + bcolors.ENDC)
+    if error: print(bcolors.FAIL + error.as_string() + bcolors.ENDC)
     else: print(bcolors.OKGREEN + str(result) + bcolors.ENDC)
